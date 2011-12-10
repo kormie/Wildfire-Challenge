983c4387ad163575b56d766f30510135464032dd
David Kormushoff <kormie@gmail.com>
2011-12-10 09:30:32 -0600

    Increase the public feed limit
    I'm not quite sure how Facebook's math works. Setting limit to 1000
    returns a respectable number of items. Also add misc. Formatting to fit
    my personal preference.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User
   end
 
   def likes_by_category
-    @likes_by_category ||= likes.sort_by {|l| l['name']}.group_by {|l| l['category']}.sort
+    @likes_by_category ||= likes.sort_by {|like| like['name']}.group_by {|like| like['category']}.sort
   end
   
   def friends
@@ -19,10 +19,12 @@ class User
   end
   
   def feed
-    @feed ||= graph.get_connections(uid, 'feed', {limit: 100})
+    @feed ||= graph.get_connections(uid, 'feed', {limit: 1000})
   end
   
   def wall_comments
-    @wall_comments ||= feed.collect{ |post| post['comments']['data'] if post['comments']['count'] > 0}.flatten.compact
+    @wall_comments ||= feed.collect do |post|
+      post['comments']['data'] if post['comments']['count'] > 0
+    end.flatten.compact
   end
 end
